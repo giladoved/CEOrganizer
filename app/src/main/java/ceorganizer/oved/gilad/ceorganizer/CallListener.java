c4983210f10f28fcd378a39e16711bcb7a85f817
Gilad Oved <giladoved@gmail.com>
2015-06-14 19:10:50 -0500

    shows hardcoded info for incoming and outgoing calls

--- app/src/main/java/ceorganizer/oved/gilad/ceorganizer/CallListener.java
+++ app/src/main/java/ceorganizer/oved/gilad/ceorganizer/CallListener.java
@@ -4,8 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.ParseObject;
 
@@ -15,6 +22,9 @@ import java.util.Date;
  * Created by gilad on 6/10/15.
  */
 public class CallListener  {
+    String incomingNum;
+    LinearLayout ly1;
+    WindowManager wm;
 
     /**
      * Listener to detect incoming calls.
@@ -23,6 +33,7 @@ public class CallListener  {
         int lastState = TelephonyManager.CALL_STATE_IDLE;
         Date callStartTime;
         boolean isIncoming;
+        boolean shouldShowProfile;
 
         //Incoming call-  goes from IDLE to RINGING when it rings, to OFFHOOK when it's answered, to IDLE when its hung up
         //Outgoing call-  goes from IDLE to OFFHOOK when it dials out, to IDLE when hung up
@@ -37,14 +48,16 @@ public class CallListener  {
                 case TelephonyManager.CALL_STATE_RINGING:
                     isIncoming = true;
                     callStartTime = new Date();
-                    //Toast.makeText(getApplicationContext(), "incoming call started from " + incomingNumber, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context, "incoming call started from " + incomingNumber, Toast.LENGTH_SHORT).show();
+                    addProfilePopup();
                     break;
                 case TelephonyManager.CALL_STATE_OFFHOOK:
                     //Transition of ringing->offhook are pickups of incoming calls.  Nothing donw on them
                     if (lastState != TelephonyManager.CALL_STATE_RINGING) {
                         isIncoming = false;
                         callStartTime = new Date();
-                        //Toast.makeText(getApplicationContext(), "outgoing call started from " + savedNumber, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(context, "outgoing call started from " + incomingNumber, Toast.LENGTH_SHORT).show();
+                        addProfilePopup();
                     }
                     break;
                 case TelephonyManager.CALL_STATE_IDLE:
@@ -54,19 +67,21 @@ public class CallListener  {
                         ParseObject summaryObj = new ParseObject("Summary");
                         summaryObj.put("type", "missed call");
                         summaryObj.put("summary", "");
-                        summaryObj.put("caller", incomingNumber);
+                        summaryObj.put("caller", incomingNum);
                         summaryObj.saveInBackground();
                         //Toast.makeText(context, "missed call from " + incomingNumber, Toast.LENGTH_SHORT).show();
                     } else if (isIncoming) {
                         //Toast.makeText(context, "incoming call ended from " + incomingNumber, Toast.LENGTH_SHORT).show();
+                        removeProfilePopup();
 
                         Intent i = new Intent(context, AddSummary.class);
                         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         i.putExtra(MainActivity.EXTRA_TYPE, "incoming call");
-                        i.putExtra(MainActivity.EXTRA_NUMBER, incomingNumber);
+                        i.putExtra(MainActivity.EXTRA_NUMBER, incomingNum);
                         context.startActivity(i);
                     } else {
                         //Toast.makeText(context, "outgoing call ended from " + outgoingNumber, Toast.LENGTH_SHORT).show();
+                        removeProfilePopup();
 
                         Intent i = new Intent(context, AddSummary.class);
                         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -78,6 +93,51 @@ public class CallListener  {
             }
             lastState = state;
         }
+
+        //http://stackoverflow.com/questions/23701879/popup-window-on-incoming-call-screen-like-truecaller
+        public void addProfilePopup() {
+            wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            WindowManager.LayoutParams params1 = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.MATCH_PARENT,
+                    WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT |
+                    WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSPARENT);
+            Display display = wm.getDefaultDisplay();
+            params1.height = display.getWidth()/4;
+            params1.width = display.getWidth();
+            params1.x = 0;
+            params1.y = -100;
+            params1.format = PixelFormat.TRANSLUCENT;
+
+            ly1 = new LinearLayout(context);
+            ly1.setBackgroundColor(Color.GRAY);
+            ly1.setOrientation(LinearLayout.VERTICAL);
+
+            TextView nameText = new TextView(context);
+            nameText.setText("John Cena");
+            ly1.addView(nameText);
+
+            TextView jobText = new TextView(context);
+            jobText.setText("Professor Wrangler");
+            ly1.addView(jobText);
+
+            TextView companyText = new TextView(context);
+            companyText.setText("Google");
+            ly1.addView(companyText);
+
+            TextView numberText = new TextView(context);
+            numberText.setText("781-308-7824");
+            ly1.addView(numberText);
+
+            wm.addView(ly1, params1);
+        }
+
+        public void removeProfilePopup() {
+            wm.removeView(ly1);
+        }
+
     }
 
     /**
@@ -88,7 +148,7 @@ public class CallListener  {
         @Override
         public void onReceive(Context context, Intent intent) {
             String number = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-            //Toast.makeText(context, "Outgoing---: "+number, Toast.LENGTH_LONG).show();
+            Toast.makeText(context, "NUM!!-: " + number, Toast.LENGTH_LONG).show();
             outgoingNumber = number;
         }
 
---
+++ app/src/main/java/ceorganizer/oved/gilad/ceorganizer/PopupProfile.java
@@ -0,0 +1,19 @@
+package ceorganizer.oved.gilad.ceorganizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class PopupProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transparent);
+
+        /*final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("your title");
+        alertDialog.setMessage("your message");
+        alertDialog.show();*/
+    }
+}
