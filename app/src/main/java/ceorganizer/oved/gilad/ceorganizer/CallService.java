325f081bdb6045eca05d7ca0cac8cc17bfd977da
Gilad Oved <giladoved@gmail.com>
2015-06-23 12:49:59 -0500

    cleaned up service code and extra activities

--- app/src/main/java/ceorganizer/oved/gilad/ceorganizer/CallService.java
+++ app/src/main/java/ceorganizer/oved/gilad/ceorganizer/CallService.java
@@ -8,7 +8,6 @@ import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.Handler;
 import android.os.IBinder;
-import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
@@ -20,7 +19,6 @@ import android.widget.TableLayout;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -45,11 +43,7 @@ public class CallService extends Service {
         Timer timer  =  new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
-                List<ActivityManager.RunningTaskInfo> taskInfo = am.getRunningTasks(1);
-
-                String currentRunningActivityName = taskInfo.get(0).topActivity.getClassName();
-                Log.d("SWAG", currentRunningActivityName);
-                if (currentRunningActivityName.equals("com.android.contacts.activities.DialtactsActivity")) {
+                if (am.getRunningTasks(1).get(0).topActivity.getClassName().equals("com.android.contacts.activities.DialtactsActivity")) {
                     handler.post(new Runnable() { // This thread runs in the UI
                         @Override
                         public void run() {
@@ -59,7 +53,7 @@ public class CallService extends Service {
                     });
                 }
             }
-        }, 0, 1000);  // every 3 seconds
+        }, 0, 5000);  // every 3 seconds
 
 
         return START_STICKY;
